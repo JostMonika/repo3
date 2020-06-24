ca4595267b2be7f7208e36df7c0f21aade116629
孙振浩 <2020400476@qq.com>
2020-06-24 10:26:26 +0800

    把本地工程，添加到git

--- src/com/itheima/git/FIrstClass.java
+++ src/com/itheima/git/FIrstClass.java
@@ -12,4 +12,9 @@ public class FIrstClass {
         System.out.println("hello git project");
         System.out.println("hello git project");
     }
+
+    public void test(){
+        int i = 0;
+        i += 10;
+    }
 }
